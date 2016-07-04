80476b8919e51fede47bf0a2ed020984138fca30
rEdonE13 <jose.a.lugo.v@gmail.com>
2016-07-04 19:36:44 +0200

    Testing android studio and github

---
+++ app/src/main/java/com/redone13/pp/loginActivity.java
@@ -0,0 +1,28 @@
+package com.redone13.pp;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class loginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final TextView registerLink = (TextView) findViewById(R.id.b_register);
+
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(loginActivity.this, registerActivity.class);
+                loginActivity.this.startActivity(registerIntent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/redone13/pp/registerActivity.java
@@ -0,0 +1,41 @@
+package com.redone13.pp;
+
+import android.app.Activity;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class registerActivity extends Activity {
+
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        spinner = (Spinner)findViewById(R.id.sp_employment_place);
+        adapter = ArrayAdapter.createFromResource(this,R.array.employment_places,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemAtPosition(position)+" selected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+}

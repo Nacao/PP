c8b929c2a3c6290cbb2e5813173c71f0407b26a8
rEdonE13 <jose.a.lugo.v@gmail.com>
2016-08-20 23:37:19 +0200

    Bottom Bar added from a Menu xml file. Three main fragments added.

--- app/src/main/java/com/redone13/pp/TestActivity.java
+++
@@ -1,7 +0,0 @@
-package com.redone13.pp;
-
-/**
- * Created by rEdonE on 8/5/2016.
- */
-public class TestActivity {
-}
---
+++ app/src/main/java/com/redone13/pp/mFragments/ParkingPlaceFragment.java
@@ -0,0 +1,7 @@
+package com.redone13.pp.mFragments;
+
+/**
+ * Created by rEdonE on 8/20/2016.
+ */
+public class ParkingPlaceFragment {
+}
